4e54124e310695ed658f6894ca596564cc49da37
masci <mpippi@gmail.com>
2012-07-19 18:50:16 +0200

    serve static file from the view

--- wsgi/pypi/views.py
+++ wsgi/pypi/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
-import os
 from django.shortcuts import render_to_response
+from django.conf import settings
+from sendfile import sendfile
+import os
 
 def home(request):
     return render_to_response('pypi/home.html')
+
+def get_package(request, path):
+    abs_path = os.path.join(settings.MEDIA_ROOT, settings.DJANGOPYPI_RELEASE_UPLOAD_TO, path)
+    return sendfile(request, abs_path)
