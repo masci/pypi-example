c4f74d0d741d7f980cb0bffee9745c39c30f3f83
masci <mpippi@gmail.com>
2012-07-19 12:24:15 +0200

    added dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='PyPi Example',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='Private PyPi on OpenShift',
+      author='Massimiliano Pippi',
+      author_email='mpippi@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Django>=1.3', 'djangopypi', 'django-sendfile'],
      )
