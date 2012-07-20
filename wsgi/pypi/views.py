de9c7ead07aea26ac9305850f66ffdcd8cfd9e54
masci <mpippi@gmail.com>
2012-07-20 17:02:12 +0200

    added view to serve media files without authentication

--- wsgi/pypi/views.py
+++ wsgi/pypi/views.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.conf import settings
+from decorators import basicauth_required
+
 from sendfile import sendfile
+
 import os
+
 
 def home(request):
     return render_to_response('pypi/home.html')
 
+@basicauth_required
+def do_auth_get_package(request, abs_path):
+    return sendfile(request, abs_path)
+
+def do_get_package(request, abs_path):
+    return sendfile(request, abs_path)
+
 def get_package(request, path):
     abs_path = os.path.join(settings.MEDIA_ROOT, settings.DJANGOPYPI_RELEASE_UPLOAD_TO, path)
-    return sendfile(request, abs_path)
+    if settings.PASSWORD_PROTECT_PYPI:
+        return do_auth_get_package(request, abs_path)
+    return do_get_package(request, abs_path)
