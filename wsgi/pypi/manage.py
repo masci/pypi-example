23395d921d1379a8af7931b891363d89a5ab0c75
masci <mpippi@gmail.com>
2012-07-20 17:52:25 +0200

    updated manage.py

--- wsgi/pypi/manage.py
+++ wsgi/pypi/manage.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
-import imp
-
-try:
-    imp.find_module('settings') # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
-    sys.exit(1)
-
-import settings
+import os, sys
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+    try:
+        # activate virtualenv
+        virtenv = os.path.join(os.environ['APPDIR'], '/virtenv/')
+        activate_this = os.path.join(virtenv, 'bin', 'activate_this.py')
+        execfile(activate_this, dict(__file__=activate_this))
+    except:
+        pass
+
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
